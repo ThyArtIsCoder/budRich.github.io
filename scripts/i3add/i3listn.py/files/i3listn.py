07b0b2d4762c31cf9842147f578d9cd2d176fb10
budRich <robstenklippa@gmail.com>
2018-01-08 16:00:10 +0100

    massive update

---
+++ scripts/i3add/i3listn.py/files/i3listn.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+from i3ipc import Connection
+from subprocess import call
+
+i3 = Connection()
+
+
+def bindnotify(i3, event):
+    # print(event.binding.__dict__)
+    bind = event.binding
+
+    mods_dict = [
+        ['Mod4', 'Super'],
+        ['shift', 'Shift'],
+        ['ctrl', 'Control'],
+        ['Mod1', 'Alt'],
+    ]
+    mods = '+'.join([
+        mod[1]
+        for mod in mods_dict
+        if mod[0] in bind.mods
+    ])
+    if mods: 
+        mods += '+'
+
+    call('notify-send -t 1 -a i3wm'.split(' ') +
+        [bind.command, '%s%s' % (mods, bind.symbol)])
+
+
+def workspacenotify(i3, event):
+    # print(event.change)
+    if event.change in "focus":
+        if event.old.num != -1:
+            call('i3wswp'.split(' ')+[format(event.current.name)])
+            call('polybar-msg hook pbws 1'.split(' '))
+
+
+def windownotify(i3, event):
+    # print(event.change)
+
+    if event.container.fullscreen_mode == 0:
+        call('polybar-msg cmd show'.split(' '))
+    else:
+        call('polybar-msg cmd hide'.split(' '))
+
+    if event.change == "focus":
+        if event.container.window_class == 'mpv':
+            call('polybar-msg hook mediatitle 2'.split(' '))
+            # call('mpc -q pause'.split(' '))
+
+    if event.change == "focus":
+        call('polybar-msg hook pbtitle 1'.split(' '))
+
+    if event.change == "title":
+        call('polybar-msg hook pbtitle 2'.split(' '))
+
+    if event.change == "close":
+        if event.container.window_class == 'mpv':
+            call('mpc -q play'.split(' '))
+            # call('polybar-msg hook mediatitle 3'.split(' '))
+
+
+    if event.change == "close":
+        if event.container.window_instance == "castterm":
+            call('pbrectime stop'.split(' '))
+
+    if event.change == "new":
+        # print(event.container.__dict__)
+        if event.container.window_instance == "castterm":
+            call('pbrectime start'.split(' '))
+
+        
+            # print('bullla')
+
+
+# i3.on('binding', bindnotify)
+i3.on('window', windownotify)
+i3.on('workspace', workspacenotify)
+
+# WORKSPACE = (1 << 0)
+# OUTPUT = (1 << 1)
+# MODE = (1 << 2)
+# WINDOW = (1 << 3)
+# BARCONFIG_UPDATE = (1 << 4)
+# BINDING = (1 << 5)
+
+i3.main()
